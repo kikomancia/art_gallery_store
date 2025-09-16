60e51a18cac1308ffe23d0246535e5603d90956e
Mikah <mikahbocarcar137@gmail.com>
2025-09-16 16:04:41 +0800

    database conn

--- php_code/dbconn.php
+++ php_code/dbconn.php
@@ -1,5 +1,5 @@
 <?php
-	$connect = mysqli_connect ('localhost', 'root', '', 'mikastore');
+	$connect = mysqli_connect ('localhost', 'u568402898_artgaldb', 'P=g:GFT#9', 'u568402898_artgaldb');
 	if ($connect->connect_error) {
 		die("connection failed : " . $connect->connect_error);
 	} else {
